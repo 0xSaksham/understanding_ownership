29bd0445c5e92a42a987aac34a8513193123570e
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-01 16:23:48 +0530

    Initial commit

---
+++ target/debug/deps/understanding_ownership-04761fea05ec191a.d
@@ -0,0 +1,5 @@
+/home/saksham/Documents/Development/Rust/understanding_ownership/target/debug/deps/understanding_ownership-04761fea05ec191a.rmeta: src/main.rs
+
+/home/saksham/Documents/Development/Rust/understanding_ownership/target/debug/deps/understanding_ownership-04761fea05ec191a.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/understanding_ownership-53b0bd9382d57508.d
@@ -0,0 +1,5 @@
+/home/saksham/Documents/Development/Rust/understanding_ownership/target/debug/deps/understanding_ownership-53b0bd9382d57508.rmeta: src/main.rs
+
+/home/saksham/Documents/Development/Rust/understanding_ownership/target/debug/deps/understanding_ownership-53b0bd9382d57508.d: src/main.rs
+
+src/main.rs:
