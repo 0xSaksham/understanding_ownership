307f0110b578d9e1baf4f9d973d57060451031e2
Saksham Gupta <sakshamgupta.ai@gmail.com>
2023-12-02 07:28:11 +0530

    Tuple Ownership Not Transfer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-fn main(){
 
+fn main() {
     println!("\n");
     // Memory Heavy Clone Method
     clone_method();
     println!("\n");
-    
+
     // Copy Trait available only for types using stack, or having known size at compile time
     stack_copy_trait();
     println!("\n");
@@ -16,51 +16,62 @@ fn main(){
 
     let x = 5;
     makes_copy(x);
-    println!("Original x(in main):{}",x);
+    println!("Original x(in main):{}", x);
     println!("\n");
 
     // Return Values and Scope
 
     let _s1 = gives_ownership();
-    
     let _s2 = String::from("Saksham");
-
     let _s3 = takes_and_gives_back(_s2);
+    println!("String 1:{} \nString 3:{}", _s1, _s3);
+    println!("\n");
+    
+    // Not giving Ownership Part-1(Tuples)
 
-    println!("String 1:{} \nString 3:{}",_s1,_s3);
+    let s1 = String::from("Krishna");
+    let (s2,len) = calculate_length(s1);
+
+    println!("The length of {} is {}",s2,len);
 
 
 }
 
-fn clone_method(){
+fn clone_method() {
     let s1 = String::from("Hello");
     let s2 = s1.clone();
 
     println!("s1 = {}, s2 = {}", s1, s2);
 }
 
-fn stack_copy_trait(){
+fn stack_copy_trait() {
     let x = 5;
     let y = x;
 
     println!("x = {}, y = {}", x, y);
 }
 
-fn takes_ownership(some_string:String){
+fn takes_ownership(some_string: String) {
     println!("{}", some_string);
 }
 
-fn makes_copy(some_int:i32){
+fn makes_copy(some_int: i32) {
     println!("{}", some_int);
 }
 
 // Return Values and Scope
-fn gives_ownership() -> String{
-    let some_str =String::from("Aloo");
+fn gives_ownership() -> String {
+    let some_str = String::from("Aloo");
 
     some_str
 }
 
-fn takes_and_gives_back(a_string:String) -> String{
+fn takes_and_gives_back(a_string: String) -> String {
     a_string
+}
+// Not giving Ownership Part-1(Tuples)
+
+fn calculate_length(s:String) -> (String,usize){
+    let len = s.len();
+    (s,len)
 }
